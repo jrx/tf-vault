3c1a7bc80278a24edc74ba631a30fe98f186a9c9
jrx <jrepnak@hashicorp.com>
2023-07-13 20:26:24 +0200

    Outputs CA file

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "vault_public_ip" {
 output "vault_lb" {
   value = module.loadbalancer.vault_lb_dns_name
 }
+
+output "ca" {
+  value     = tls_self_signed_cert.ca.cert_pem
+  sensitive = true
+}
