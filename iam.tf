71d225d8f8240ab1e6a57ca072c2d01e5e32bee6
jrx <jrepnak@hashicorp.com>
2023-10-20 16:01:40 +0200

    Adds IAM permission to sync secrets to the AWS Secrets Manager

--- iam.tf
+++ iam.tf
@@ -17,6 +17,13 @@ resource "aws_iam_role" "vault_kms_unseal_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_iam_instance_profile" "vault_profile" {
+  name = "${var.cluster_name}-vault_profile"
+  role = aws_iam_role.vault_kms_unseal_role.name
+}
+
+# Auto-unseal
+
 data "aws_iam_policy_document" "vault-kms-unseal" {
   statement {
     sid       = "VaultKMSUnseal"
@@ -32,18 +39,47 @@ data "aws_iam_policy_document" "vault-kms-unseal" {
   }
 }
 
-resource "aws_iam_policy" "vault-policy" {
-  name   = "${var.cluster_name}-vault-policy"
+resource "aws_iam_policy" "vault-policy-kms-unseal" {
+  name   = "${var.cluster_name}-vault-policy-kms-unseal"
   policy = data.aws_iam_policy_document.vault-kms-unseal.json
 }
 
-resource "aws_iam_policy_attachment" "vault-attach" {
-  name       = "${var.cluster_name}-vault-attach"
+resource "aws_iam_policy_attachment" "vault-attach-kms" {
+  name       = "${var.cluster_name}-vault-attach-kms"
   roles      = [aws_iam_role.vault_kms_unseal_role.id]
-  policy_arn = aws_iam_policy.vault-policy.arn
+  policy_arn = aws_iam_policy.vault-policy-kms-unseal.arn
 }
 
-resource "aws_iam_instance_profile" "vault_profile" {
-  name = "${var.cluster_name}-vault_profile"
-  role = aws_iam_role.vault_kms_unseal_role.name
+# Secrets Sync
+
+data "aws_iam_policy_document" "vault-secrets-sync" {
+  statement {
+    sid       = "SecretsManagerAccess"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:CreateSecret",
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:UpdateSecret",
+      "secretsmanager:UpdateSecretVersionStage",
+      "secretsmanager:DeleteSecret",
+      "secretsmanager:RestoreSecret",
+      "secretsmanager:TagResource",
+      "secretsmanager:UntagResource"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "vault-policy-secrets-sync" {
+  name   = "${var.cluster_name}-vault-policy-secrets-sync"
+  policy = data.aws_iam_policy_document.vault-secrets-sync.json
+}
+
+resource "aws_iam_policy_attachment" "vault-attach-secrets-sync" {
+  name       = "${var.cluster_name}-vault-attach-secrets-sync"
+  roles      = [aws_iam_role.vault_kms_unseal_role.id]
+  policy_arn = aws_iam_policy.vault-policy-secrets-sync.arn
 }
